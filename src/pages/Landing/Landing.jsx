743762ae43e7b085d15a48e2cd177cac61affc43
Ashlc <isadoabpaz@gmail.com>
2023-07-26 19:53:27 -0300

    :wrench: Setting up for development.

---
+++ src/pages/Landing/Landing.jsx
@@ -0,0 +1,15 @@
+import "./Landing.css";
+import React from "react";
+
+function Landing() {
+  return (
+    <div className="flex justify-center w-full mt-10">
+      <div>
+        <p className="text-3xl text-red-500 font-bold">LANDING PAGE</p>
+        <p className="text-md text-slate-500 text-center mt-10">WOW!</p>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
