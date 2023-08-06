bb7295b1f68e031020b7cfc3f2fe908724774995
Ashlc <isadoabpaz@gmail.com>
2023-08-06 15:37:58 -0300

    Landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,12 +1,20 @@
 import "./Landing.css";
 import React from "react";
+import logo from "../../assets/RUPLUS.svg"
 
 function Landing() {
   return (
-    <div className="flex justify-center w-full mt-10">
-      <div>
-        <p className="text-3xl text-red-500 font-bold">LANDING PAGE</p>
-        <p className="text-md text-slate-500 text-center mt-10">WOW!</p>
+    <div className="flex justify-center w-full h-screen">
+      <div className="flex flex-col justify-center items-center text-center gap-10">
+        <div className="gap-2 flex flex-col justify-center items-center">
+          <img src={logo} className="w-36" />
+          <p className="font-medium text-white">Carteira Virtual</p>
+        </div>
+        <div className="flex flex-col justify-center items-center gap-3">
+          <input type="email" placeholder='Email' className="leading-none rounded-lg border-silver border w-72 pl-3 py-1 focus:outline-moonstone" />
+          <input type="password" placeholder='Senha' className="leading-none rounded-lg border-silver border w-72 pl-3 py-1 focus:outline-moonstone" />
+          <a href=""><p className="text-white text-sm underline underline-offset-4">Esqueceu sua senha?</p></a>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'stdblue': '#005CA9',
+        'silver': '#BDC3C7',
+        'moonstone': '#2571CF',
+      }
+    },
   },
   plugins: [],
 }
