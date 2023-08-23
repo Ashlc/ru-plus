2f034ca35d7c7da68e70c33cf4c0a812b824f8cc
unknown <gustavodomingosdeoliveira@hotmail.com>
2023-08-23 19:22:44 -0300

    :sparkles: feat: Tela de Pagamento fail e Pagamento QR Code

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from './pages/Landing/Landing.jsx'
 import Payment2 from './pages/Payment/Payment2.jsx'
+import Payment1 from './pages/Payment/Payment1.jsx'
+import PaymentFail from './pages/Payment/FailedPayment.jsx'
 
 function App() {
   return (
@@ -9,7 +11,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />}>
         </Route>
-        <Route path="/payment" element={<Payment2 />}>
+        <Route path="/payment2" element={<Payment2 />}>
+        </Route>
+        <Route path="/payment1" element={<Payment1  />}>
+        </Route>
+        <Route path="/payment-fail" element={<PaymentFail  />}>
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/Payment/Payment2.jsx
+++ src/pages/Payment/Payment2.jsx
@@ -16,12 +16,14 @@ function Payment2() {
       </div>
       <div className="flex flex-col justify-center items-center gap-3">
         <p className="font-bold text-white mulish-font text-xl">{mealType}</p>
-        <p className="font-bold text-white mulish-font text-4xl">R$ {price.toFixed(2).replace('.', ',')}</p>
-        
+        <p className="font-bold text-white mulish-font text-4xl">
+        <span className="big-text font-weigt-800">R$ </span>
+        <span>{price.toFixed(2).replace('.', ',')}</span>
+         </p>
       </div>
       <div className="flex flex-col justify-center items-center">
         {/*Currently this goes back to the start, when we make the correct path I will change this*/}
-        <Link to="/" className="text-white text-sm underline underline-offset-4 mt-4 mulish-font">Voltar</Link>
+        <Link to="/" className="fifteen-text voltar mt-20 mulish-font">Voltar</Link>
       </div>
     </div>
   </div>
