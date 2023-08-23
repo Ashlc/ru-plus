edaf01fbc965837e57f9c8bce5ccf70a8681f6d8
Ashlc <isadorabpaz@gmail.com>
2023-08-23 09:23:21 -0300

    Finishing home screen and installed the QR code library.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import Navigator from "../../components/Navigator/Navigator";
 import Saldo from "./Saldo";
 import Extract from "./Extract";
+import IconButton from "../../components/IconButton/IconButton";
 
 function Home() {
 	return (
-		<div className="h-screen flex flex-col justify-center items-center gap-5">
-			<Saldo />
+		<div className="h-screen flex flex-col justify-center items-center gap-5 pb-10">
+			<div className="w-4/5 flex flex-col justify-center items-center gap-8 h-full">
+				<Saldo />
+				<Extract />
+				<IconButton text="INSERIR CRÉDITOS" icon="pi-credit-card" />
+			</div>
 			<Navigator />
-			<Extract />
 		</div>
 	);
 }
--- src/pages/Home/Saldo.jsx
+++ src/pages/Home/Saldo.jsx
@@ -2,15 +2,13 @@ import React from "react";
 
 export default function Saldo() {
 	return (
-		<div className="h-1/5 w-3/4 border border-silver rounded-2xl flex flex-col">
-			<div className="pt-3 pl-4">
+		<div className="h-1/5 w-full relative border border-silver rounded-2xl flex flex-col items-center justify-center gap-2">
+			<div className="absolute top-3 left-4">
 				<p className="text-stdblue font-bold text-md">SALDO</p>
 			</div>
-			<div className="flex items-center justify-center pt-3">
-				<p className="text-silver text-4xl font-medium">
-					R$ <b className="text-midnight">00</b>,00
-				</p>
-			</div>
+			<p className="text-silver text-4xl font-medium">
+				R$ <b className="text-midnight">00</b>,00
+			</p>
 		</div>
 	);
 }
